ecb843b69ec3122cb302de2a96cbade6f1c296a4
Anna Zajac <aniazajac9112@gmail.com>
2021-12-03 17:55:29 +0100

    Add variable playerMove

--- js/script.js
+++ js/script.js
@@ -1,9 +1,2 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+let playerMove='papier';
+printMessage ('Zagrałem'+'playerMove'+'!Jeśli Twój ruch to nożyce, to wygrywasz!');
